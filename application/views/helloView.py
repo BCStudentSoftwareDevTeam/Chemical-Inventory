a3ccaa4f7f3c4f4a9b4c29c424bf0491be3fe06a
Matt Jadud <matt@jadud.com>
2016-06-08 22:07:28 -0400

    Setting up views.
    Added a title for the application.
    
    Modified the view to render a template.

--- application/views/helloView.py
+++ application/views/helloView.py
@@ -1,6 +1,7 @@
 from application import app
 from config import *
+from flask import render_template
 
 @app.route('/')
 def index():
-  return "Hello"
+  return render_template("hello.html", config = config)
