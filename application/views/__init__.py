d054e423c1b1383f06ff70cf50176719b49fbe4d
Matt Jadud <matt@jadud.com>
2016-06-08 22:14:48 -0400

    Removing debug statements.

--- application/views/__init__.py
+++ application/views/__init__.py
@@ -2,10 +2,10 @@ import glob, os
 
 views = []
 
-print "cwd: {0}".format(os.getcwd())
+# print "cwd: {0}".format(os.getcwd())
 for file in glob.glob("application/views/*View.py"):
-    print "File: {0}".format(file)
+    # print "File: {0}".format(file)
     views.append(os.path.splitext(os.path.basename(file))[0])
 
-print "Found views: {0}".format(views)
+# print "Found views: {0}".format(views)
 __all__ = views
