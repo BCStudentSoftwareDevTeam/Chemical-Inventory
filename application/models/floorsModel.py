e283cc14bd9353f1e98ab15422d5d7023b4088ee
Zach Ball <ballz@berea.edu>
2016-06-24 12:45:39 +0000

    Cleaning up files after recreating with different names

--- application/models/floorsModel.py
+++ application/models/floorsModel.py
@@ -5,7 +5,7 @@ class Floors (Model):
   fId          = PrimaryKeyField()
   buildid       = ForeignKeyField(Buildings)
   floornum      = TextField()
-  storagelimits = TextField() # This needs to be populated with real storage limits
+  storagelimits = TextField() # This is additional functionality that does not need to be in the initial system
 
   class Meta:
     database = getDB("inventory")
