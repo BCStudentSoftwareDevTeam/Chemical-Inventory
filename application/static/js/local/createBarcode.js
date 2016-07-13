d399ab6ff2d5d8b9be87161d5ef11a1f9adb3cfc
Zach Ball <ballz@berea.edu>
2016-07-13 14:21:14 +0000

    Basic addContainer page completed. Form still needs to create the barcode

---
+++ application/static/js/local/createBarcode.js
@@ -0,0 +1,4 @@
+function createBarcode(elementId){
+    // Need to build code that creates barcodes based on the last barcode created
+    document.getElementById(elementId).placeholder="This Is Barcode!";
+}
