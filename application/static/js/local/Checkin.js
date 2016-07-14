28f04052dc6962b270e472b5ff8c9e95209759f3
Raunak Thakur <thakurr@berea.edu>
2016-07-14 20:23:30 +0000

    Added file to create dynamic search bar

--- application/static/js/local/Checkin.js
+++ application/static/js/local/Checkin.js
@@ -3,8 +3,10 @@ function CheckInModal(name,barcode,lastroom,newQuanity,newroom){
    var check = checkValues(list);
    if (check == true) {
      $("#myModal").modal();
-     //console.log("opened modal");
+     
+     
    }
+   showModal()
 }
 
 function checkValues(list){
@@ -17,3 +19,16 @@ function checkValues(list){
   }
   return true;
 }
+ function print(name,barcode,lastroom,newQuanity,newroom){
+     var Name = document.getElementById(name).textContent;
+     var Barcode = document.getElementById(barcode).textContent;
+     var Lastroom = document.getElementById(lastroom).textContent;
+     var Quanity = document.getElementById(newQuanity).textContent;
+      $('.share_list_popup ul').append(Name);
+      $('.share_list_popup ul').append(Barcode);
+
+  function showModal($) {
+      $('#myModal').modal('show');
+      //still need to fix this I have no idea how to print this on the modal
+  }
+ }
