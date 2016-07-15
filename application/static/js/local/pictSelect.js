ee40498780bb9e9cc72404500d556c9595a6eb76
Zach Ball <ballz@berea.edu>
2016-07-15 10:38:20 +0000

    Started to work on allowing multiple pictograms to be selected for one chemical

--- application/static/js/local/pictSelect.js
+++ application/static/js/local/pictSelect.js
@@ -1,7 +1,10 @@
 function pictSelect(element)
 {
-    $(".hazardRadio").removeClass('selectedPict');
-    $(element).addClass('selectedPict');
+    if (document.getElementById(element).class == 'selectedPict'){
+        document.getElementById(element).removeClass('selectedPict');  
+    }else{
+        document.getElementById(element).addClass('selectedPict');
+    }    
     document.getElementById("addChemSubmit").disabled = false;
     document.getElementById("addChemSubmit").className = "btn btn-success btn";
 }
