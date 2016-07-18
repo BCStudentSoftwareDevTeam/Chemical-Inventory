d4a8d5579377e9868d131308adee000ec592e2bc
Zach Ball <ballz@berea.edu>
2016-07-18 17:00:26 +0000

    Fixed: URL escape characters now taken care of for all users

--- application/controllers/sa/ChemTableController.py
+++ application/controllers/sa/ChemTableController.py
@@ -3,6 +3,7 @@ from application.models.chemicalsModel import *
 from application.models.containersModel import *
 from application.config import *
 from application.logic.validation import require_role
+from urllib import *
 
 from flask import \
     render_template, \
@@ -27,5 +28,6 @@ def saChemTable():
   return render_template("views/sa/ChemTableView.html",
                           config = config, 
                           chemicals = chemicals, 
-                          contDict = contDict)
+                          contDict = contDict,
+                          quote = quote)
 
