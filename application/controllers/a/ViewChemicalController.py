9a8cbaa8e2545e166d7cbc7b08c91d82ff7f96af
Zach Ball <ballz@berea.edu>
2016-07-19 15:14:28 +0000

    Changed query for containers on non-admin, view chemical controller in order to not show disposed of containers. Also, changed viewChemicalSnip to match non-admin aspects of adminViewChemicalSnip

--- application/controllers/a/ViewChemicalController.py
+++ application/controllers/a/ViewChemicalController.py
@@ -18,7 +18,10 @@ def aViewChemical(chemical, chemId):
   containers = (((Containers
                 .select())
                 .join(Chemicals))
-                .where(Containers.chemId == chemId))
+                .where(
+                  (Containers.chemId == chemId) &
+                  (Containers.disposalDate == None)
+                ))
   return render_template("views/a/ViewChemicalView.html",
                          config = config,
                          chemInfo = chemInfo,
