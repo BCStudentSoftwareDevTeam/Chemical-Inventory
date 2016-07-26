63de95f7734ed4ff6e5d409f92bb6fd0fff2143f
Zach Ball <ballz@berea.edu>
2016-07-26 15:28:08 +0000

    FIXED: Only name is required when creating a chemical

--- application/models/chemicalsModel.py
+++ application/models/chemicalsModel.py
@@ -4,30 +4,30 @@ class Chemicals (Model):
   chemId          = PrimaryKeyField()
   ## General Information
   name            = CharField(unique = True)
-  casNum          = CharField(null = False)
-  primaryHazard   = CharField(null = False)
-  formula         = CharField(null = False)
+  casNum          = CharField(null = True)
+  primaryHazard   = CharField(null = True)
+  formula         = CharField(null = True)
   concentration   = CharField(null = True)                   #This field can be left empty
-  state           = CharField(null = False)
-  structure       = CharField(null = False)       # Organic or Inorganic
+  state           = CharField(null = True)
+  structure       = CharField(null = True)       # Organic or Inorganic
   flashPoint      = DecimalField(null = True)
   boilPoint       = DecimalField(null = True)
   storageTemp     = DecimalField(null = True)
   sdsLink         = CharField(null = True)
   description     = CharField(default = "")
   ## NFPA "Fire Diamond"
-  healthHazard    = CharField(null = False) # 0-4
-  flammable       = CharField(null = False) # 0-4
-  reactive        = CharField(null = False) # 0-4
+  healthHazard    = CharField(null = True) # 0-4
+  flammable       = CharField(null = True) # 0-4
+  reactive        = CharField(null = True) # 0-4
   other           = CharField(null = True) 
   ## NFPA "Fire Diamond" DATABASE DOES NOT SUPPORT THESE YET!
   #simpleAsphyxiant= BooleanField(default = False) # Simple Asphyxiant
   #oxidizer        = BooleanField(default = False) # Oxidizer
   waterReactive   = BooleanField(default = False) # Water Reactive
   ## HMIS Color Bar
-  hmisHealth      = CharField(null = False) # 0-4
-  hmisFlammable   = CharField(null = False) # 0-4
-  hmisPhysical    = CharField(null = False) # 0-4
+  hmisHealth      = CharField(null = True) # 0-4
+  hmisFlammable   = CharField(null = True) # 0-4
+  hmisPhysical    = CharField(null = True) # 0-4
   hmisPPE         = CharField(default = "A") # "A, B, C, D, E, H"
   # Ask the user which pictograms a chemical should have
   hhPict          = BooleanField(default = False) # Health Hazard Pictogram
