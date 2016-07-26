e21a8f08264891d890a159eca4ef79b547331344
Zach Ball <ballz@berea.edu>
2016-07-26 14:25:40 +0000

    I changed a lot since the last commit. I know that the barcode checker on checkin now only runs if an 8 digit number is entered, and the bootstrap theme was changed from yeti to readable in hopes of fixing the 'Bolder visuals' issue.

--- application/controllers/ma/CheckInController.py
+++ application/controllers/ma/CheckInController.py
@@ -1,11 +1,13 @@
 from application import app
-from application.models import *
+from application.models.chemicalsModel import *
+from application.models.containersModel import *
 from application.config import *
 from application.logic.validation import require_role
 
 from flask import \
     render_template, \
     request, \
+    json as simplejson, \
     url_for
 
 # PURPOSE: CheckIn a container
@@ -17,5 +19,14 @@ def maCheckIn():
     return render_template("views/ma/CheckInView.html",
                            config = config,
                            contConfig = contConfig,
-                           checkInConfig = checkInConfig)
+                           checkInConfig = checkInConfig,
+                           container = None)
   
+@app.route('/checkInData/', methods = ['GET'])
+def checkInData():
+    barId = request.args.get('barId')
+    container = Containers.select().where(Containers.barcodeId == barId).dicts().get()
+    # chemical = Chemicals.select().join(Containers).where(Containers.barcodeId == barId).dicts().get()
+    return render_template("snips/setVars.html",
+                           avatar_url = process_data(data),
+                           container = container)
--- application/static/js/local/changeForm.js
+++ application/static/js/local/changeForm.js
@@ -2,7 +2,7 @@ function changeForm(status){
     var inputList = document.getElementsByClassName("d");
     for (var i = 0; i < inputList.length; i++){
         if (status == true){
-           inputList[i].value = '';   
+           inputList[i].value = '';
         }
         inputList[i].disabled = status;
     }
@@ -10,9 +10,25 @@ function changeForm(status){
 
 function checkBar(){
     var barInput = document.getElementById('barcodeId');
-    if (barInput.value != ''){
-        changeForm(false)
+    if (barInput.value.length == 8){
+        changeForm(false);
+        getData(barInput);
     }else{
         changeForm(true);
     }
+}
+
+function getData(barcodeId){
+    $.getJSON("/checkInData/", {
+        barId: $('input[name="barcodeId"]').val()
+    },
+        function(data) {
+            console.log(data);
+            for (var contKey in data){
+                var cont = data[contKey];
+                window.chemId = "{{ group.id['" + cont.chemId + "']|safe }}";
+                window.storageId = "{{ group.id['" + cont.storageId + "'] | safe }}";
+                window.currentQuantity = cont.currentQuantity + " " + cont.currentQuantityUnit;
+            }
+        });
 }
