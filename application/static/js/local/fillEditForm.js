522595f210a7f10aacb3c4854d02b1a6f150c9cb
Zach Ball <ballz@berea.edu>
2016-08-02 13:48:49 +0000

    Working on adding functionality to edit chemical form

---
+++ application/static/js/local/fillEditForm.js
@@ -0,0 +1,6 @@
+function fillEditForm(chemId, config){
+    config = config.replace(/'/g, '"');
+    // config = JSON.parse(config);
+    console.log(config.charAt(3191));
+    console.log(config);
+}
