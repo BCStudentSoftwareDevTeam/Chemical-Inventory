5c1b4273923bb390f2ca8746b5cb23b3fc214a58
Zach Ball <ballz@berea.edu>
2016-08-03 14:45:42 +0000

    Made sure that all javascript files were properly commented.

--- application/static/js/local/changeForm.js
+++ application/static/js/local/changeForm.js
@@ -1,35 +1,35 @@
 function changeForm(status){
     var inputList = document.getElementsByClassName("d");
-    for (var i = 0; i < inputList.length; i++){
+    for (var i = 0; i < inputList.length; i++){ //For all elements with class "d"
         if (status == true){
-           inputList[i].value = '';
+           inputList[i].value = ''; //If status is true, change values of all fields to an empty string. When barcode is removed, values don't remain in text boxes.
         }
-        inputList[i].disabled = status;
+        inputList[i].disabled = status; //change disabled value to whatever is passed in. If true, they are disabled. If false, they are not.
     }
 }
 
 function checkBar(){
-    var barInput = document.getElementById('barcodeId');
-    if (barInput.value.length == 8){
-        changeForm(false);
-        getData(barInput);
+    var barInput = document.getElementById('barcodeId').value;
+    if (barInput.length == 8){ //Only runs after user has entered 8 digits into barcode field (all barcodes will be 8 digits)
+        changeForm(false); //Enable fields
+        getData(barInput); //Fill enabled fields
     }else{
-        changeForm(true);
+        changeForm(true); //Disable and clear fields
     }
 }
 
 function getData(barcodeId){
-    $.ajax({
+    $.ajax({ //AJAX call to url "/checkInData/" to get info with barcodeId that is passed in from checkBar function
         url: "/checkInData/",
-        data: {barId : barcodeId.value},
+        data: {barId : barcodeId},
         type: "GET",
-        success: function(data) {
+        success: function(data) { //fill values of elements: 'chemId', 'prevStorageId', and 'prevQuantity' with info from database
             document.getElementById("chemId").value = data['chemName'];
             document.getElementById('prevStorageId').value = data['storage'];
             document.getElementById('prevQuantity').value = data['quantity'] + " " + data['unit'];
         },
         error: function() {
-            console.log("Error: There are no containers with that barcode in the system.")
+            console.log("Error: There are no containers with that barcode in the system.") //TODO: change to write to a log file as well as console.
         }
     });
     }
--- application/static/js/local/fillEditForm.js
+++ application/static/js/local/fillEditForm.js
@@ -1,37 +1,33 @@
 function fillEditForm(chemId, config){
-    $.ajax({
+    $.ajax({ //AJAX call to get data from url "/getEditData" with chemId that is passed in from function call
         url: "/getEditData/",
         data: {chemId : chemId},
         type: "GET",
         success: function(data) {
-            config = config.replace(/'/g, '"');
-            config = config.replace(/u"/g, '"');
-            config = JSON.parse(config);
-            console.log(config);
+            config = config.replace(/'/g, '"'); //JSON will not parse with single quotes, replacing them with double quotes
+            config = config.replace(/u"/g, '"'); //Getting rid of all unicode leading characters.
+            config = JSON.parse(config); //Turning string from config file into a JSON object
             for (var section in config){
                 for (var element in config[section]){
                     var curElement = config[section][element];
                     if (curElement.type == "text" || curElement.type == "number"){
-                        document.getElementById(curElement.id).value = data[curElement.id];
+                        document.getElementById(curElement.id).value = data[curElement.id]; //Setting values of text and number fields to values from database
                     }
                     else if (curElement.type == "dropdown"){
                         var dropdownList = document.getElementById(curElement.id);
-                        dropdownList.value = data[curElement.id]
+                        dropdownList.value = data[curElement.id] //Setting selected values of dropdowns to values from database
                     }
                     else if (curElement.type == "checkbox"){
                         var checkbox = document.getElementById(curElement.id);
                         if (data[curElement.id] == 'True'){
-                            checkbox.checked = true;
-                        }
-                        else {
-                            checkbox.checked = false;
+                            checkbox.checked = true; //Setting selected boxes to true if they are set to true in database
                         }
                     }
                 }
             }
         },
         error: function(){
-            console.log("Error: Could not retrieve data for requested chemical");
+            console.log("Error: Could not retrieve data for requested chemical"); //TODO: change this to write to a log file as well as console
         }
     });
 }
