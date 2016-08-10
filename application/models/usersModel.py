9a48de11005256323fe68cecc05c324fae763b52
Raunak Thakur <thakurr@berea.edu>
2016-08-10 17:47:47 +0000

    added customer care email to the footer and then workedon the user model

--- application/models/usersModel.py
+++ application/models/usersModel.py
@@ -3,10 +3,11 @@ from application.models.util import *
 class Users (Model):
     userId = PrimaryKeyField()
     username = TextField(null = False)
-    auth_level = TextField(default = "student")
+    auth_level = TextField(null = False)
     emailadd = TextField(null = False)
     approve = BooleanField(default = False)
-    start_date = DateTimeField(null = False)
+    start_date = DateTimeField(null = True)
+    end_date = DateTimeField(null = True)
     reportto = TextField(null = False)
     created_by = TextField(null = True)
     
