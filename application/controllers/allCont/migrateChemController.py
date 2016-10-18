56c90139be7b0cce7e9312ca1be0b536460eeda4
eykrevooh <hooverk@berea.edu>
2016-10-18 14:25:12 -0400

    Built out migrateChem chem page further

--- application/controllers/allCont/migrateChemController.py
+++ application/controllers/allCont/migrateChemController.py
@@ -3,29 +3,54 @@ from application.models.staticModels.batchModel import *
 from application.models.staticModels.mainModel import * 
 from application.models.staticModels.locatesModel import *
 from application.models.chemicalsModel import *
+from application.models.containersModel import *
 from application.config import *
+from application.logic.getAuthUser import AuthorizedUser
 
 
 from flask import render_template, \
                           request, \
                           jsonify
 
-@app.route('/ma/migrateChem/', methods = ['GET', 'POST'])
-def maMigrateChem():
-    containerObj = None
+@app.route('/migrateChem/', methods = ['GET', 'POST'])
+def migrateChem():
+    auth = AuthorizedUser()
+    user = auth.getUser()
+    userLevel = auth.userLevel()
+    print user.username, userLevel
 
     #locdict = Batch.select().dicts().get() This was used for datamodel testing
-    if request.method == "POST":
-        #Gets container from old DB by input barcode whether lower or upper case
-        try:
-            containerObj = Batch.select().join(Main, on =(Batch.NameRaw_id == Main.NameSorted)).join(Locates, on=(Batch.Id_id == Locates.Location)).where((Batch.UniqueContainerID == request.form['barcodeID'])|(Batch.UniqueContainerID == request.form['barcodeID'].upper())).get()
-        except:
-            pass
-            #Checks if chemical already in system
-        try:
-            pass
-            ###########You left off here. You are checking if chemical exists is both systems##
-            containerObj.IsSupplyItem
-        except:
-            pass
-    return render_template("views/ma/MigrateChem.html", container = containerObj, config = config)
+    if userLevel == 'admin' or userLevel == "systemAdmin":
+        if request.method == "GET":
+            return render_template("views/RequestMigrateChem.html",
+                    config = config)
+    
+
+        elif request.method == "POST":
+            containerObj = None
+            alreadyMoved = False
+            #Gets container from new DB by input barcode whether lower or upper case
+            try:
+                containerObj = Containers.select()\
+                        .join(Chemicals, on=(Containers.chemId_id == Chemicals.chemId))\
+                        .where((Containers.barcodeId == request.form['barcodeID'])\
+                        |(Containers.barcodeId == request.form['barcodeID'].upper()))\
+                        .get()
+                alreadyMoved = True
+            except Exception,e:
+                print str(e)
+                pass
+            if alreadyMoved == False:
+                #If not in new DB. Gets the container from old DB by input barcode whether lower or upper case
+                try:
+                    containerObj = Batch.select()\
+                            .join(Main, on =(Batch.NameRaw_id == Main.NameSorted))\
+                            .join(Locates, on=(Batch.Id_id == Locates.Location))\
+                            .where((Batch.UniqueContainerID == request.form['barcodeID'])|(Batch.UniqueContainerID == request.form['barcodeID'].upper()))\
+                            .get() 
+                except:
+                    pass
+            return render_template("views/MigrateChem.html",
+                            alreadyMoved = alreadyMoved,
+                            container = containerObj, 
+                            config = config)
