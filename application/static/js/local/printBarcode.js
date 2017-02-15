697294adc7ffdb63aa2bce7305f8e774b1dbf95c
Zach Ball <ballz@berea.edu>
2017-02-15 22:43:46 +0000

    Added a print button to each row of the container list for a chemical. Also fixed how each row is linked to accomadate for the new print button.

---
+++ application/static/js/local/printBarcode.js
@@ -0,0 +1,25 @@
+function printBarcode(barId){
+    // e.stopImmediatePropogation();
+    var barcode = document.getElementById(barId);
+    var openWindow = window.open("", "", "width=800, height=700");
+    openWindow.document.write("<title>Print Barcode</title>");
+    openWindow.document.write(barcode.innerHTML);
+    openWindow.document.close();
+    openWindow.focus();
+    openWindow.print();
+    openWindow.close();
+}
+
+$(document).ready(function(){
+    $('#containers tr').on('click', function(e){
+        if ( $(e.target).is('span') ){ 
+            printBarcode($(e.target.id).selector)
+        }
+        else { //If anywhere else in the row was clicked
+            var href = $(this).find("a").attr("href"); //Set the link to the correct one
+            if(href){
+                window.location = href; //And change the window location to that link
+            }
+        }
+    })
+})
