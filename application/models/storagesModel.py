f301f4ab07eb758573e999418c82f5628c2f7ef2
eykrevooh <hooverk@berea.edu>
2017-03-14 13:53:35 -0400

    Readded OldPK to model to fix pop_sample

--- application/models/storagesModel.py
+++ application/models/storagesModel.py
@@ -4,6 +4,7 @@ from application.models.roomsModel import Rooms
 class Storages(Model):
   sId           = PrimaryKeyField()
   roomId        = ForeignKeyField(Rooms) # When creating a container, select room first, then populate dropdown with all storages with matching roomId.
+  oldPK         = IntegerField(null = True)
   name          = TextField() # Name of the specific storage unit ex: "Flammable Cabinet"
   # Booleans of true are what the storage is allowed to hold
   flammable     = BooleanField(default = False)
