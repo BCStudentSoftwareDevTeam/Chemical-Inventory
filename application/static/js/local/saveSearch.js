bcb1a1966d7202eae57b13c33a1a75ff12b7435b
Cody Myers <codymyers63@gmail.com>
2017-07-06 19:49:27 +0000

    For Ben: I have implemented the search functionality for cookies. Your Welcome Ben.

--- application/static/js/local/saveSearch.js
+++ application/static/js/local/saveSearch.js
@@ -1,4 +1,7 @@
-var table = $('#chemtable').DataTable({
+var table;
+var cookie;
+function createTable(){
+    table = $('#chemtable').DataTable({
 	    "columnDefs": [
 		{
 		    "targets": 'Barcodes',
@@ -10,8 +13,10 @@ var table = $('#chemtable').DataTable({
 		}
 	                  ]
    });
- var inputSearch = document.getElementsByTagName("input")[0];
-inputSearch.setAttribute('id','myId') 
+  var inputSearch = document.getElementsByTagName("input")[0];
+  inputSearch.setAttribute('id','myId')
+  getCookie('savedSearch')
+}
 
 function cookieSaveAndRedirect(href) {
 
@@ -25,11 +30,10 @@ function cookieSaveAndRedirect(href) {
 
 
 function getCookie(cname) {
-
     //function obtained from https://www.w3schools.com/js/js_cookies.asp
     //takes cookie name and returns the key value, else it returns an empty string
     var cookie_exist = find_cookie(cname);
-    if (cookie_exist)
+    if (cookie_exist){
  	var name = cname + "=";
 	var decodedCookie = decodeURIComponent(document.cookie);
 	var ca = decodedCookie.split(';');
@@ -39,15 +43,16 @@ function getCookie(cname) {
 		    c = c.substring(1);
 		}
 		if (c.indexOf(name) == 0) {
-		    var cookie = c.substring(name.length, c.length)
-                    var el = document.getElementsByTagName("input")[0];
-                    el.value = cookie;
-		    reload_table()
-		    return True;
+		    cookie = c.substring(name.length, c.length);
+		    console.log(cookie);
+            var el = document.getElementsByTagName("input")[0];
+            el.value = cookie;
+		    reload_table(cookie)
+		    return true;
 		}
-	    }
-  
-	    return "";	
+ 	    }
+    }  
+    return "";	
 }
 
 function find_cookie ( cookie_name ){
@@ -55,14 +60,11 @@ function find_cookie ( cookie_name ){
   if ( results )
     return ( unescape ( results[2] ) );
   else
-    return False;
+    return false;
 }
 
-function reload_table(){
-	table.ajax.reload( function ( json ) {
-	    $('#myId').val( json.lastInput );
-} );
-
-
-
+function reload_table(cookie){
+    table
+         .search(cookie)
+         .draw();
 }
