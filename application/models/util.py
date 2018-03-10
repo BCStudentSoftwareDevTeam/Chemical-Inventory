0211a453bc6156ca00266d5a5f5b47c94e22fc7b
Ben <benrquesada@gmail.com>
2018-03-10 16:43:42 -0500

    added functionality to opertae using mysql, some parameters need to be changed once mysql is installed based on what you set up

--- application/models/util.py
+++ application/models/util.py
@@ -3,14 +3,24 @@ from application.absolutepath import getAbsolutePath
 from peewee import *
 
 def getDB (dbName, dbType):
-  dbPath = config.databases[dbType][dbName].filename
-  dbPath = getAbsolutePath(dbPath)
-  # print "DB Name: {0}\nDB Path: {1}".format(dbName, dbPath)
-  theDB = SqliteDatabase (dbPath,
-                          pragmas = ( ('busy_timeout',  100),
-                                      ('journal_mode', 'WAL')
-                                  ),
-                          threadlocals = True)
+  dbtype = "sqlite"
+
+  if dbtype == "mysql":
+      #theDB = MySQLDatabase('chemical')
+      theDB = MySQLDatabase('chemical', user='root', password='root',
+                             host='localhost', port=3306)
+
+  else:
+      dbPath = config.databases[dbType][dbName].filename
+      dbPath = getAbsolutePath(dbPath)
+      theDB = SqliteDatabase (dbPath,
+                              pragmas = ( ('busy_timeout',  100),
+                                          ('journal_mode', 'WAL')
+                                      ),
+                              threadlocals = True)
+
+
+
+
   config.databases[dbType][dbName].theDB = theDB
   return theDB
-
