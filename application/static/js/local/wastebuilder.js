2a3177f1f0f2bd26e3d5990240594d412372a0d4
Ben <benrquesada@gmail.com>
2018-04-19 11:49:11 -0400

    added tabs to waste management page and added js file for adding waste chemicals, this will need more editing

---
+++ application/static/js/local/wastebuilder.js
@@ -0,0 +1,10 @@
+var wastecontent = [];
+
+function addWasteChem(wID, wCHEMID){
+  var chem = {
+    "wID" = wID,
+    "wCHEMID" = wCHEMID
+    "deleted", = 0
+  }
+  wastecontent.append(chem) //check this
+}
