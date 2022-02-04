9861778f2179384793353de7bcdfbc61378d4bb5
Hila Manalai <manalaih@berea.edu>
2022-02-04 10:48:08 -0500

    trying to get user ip address

--- run.py
+++ run.py
@@ -3,5 +3,11 @@ from application import app
 # Import all of our configuration
 from application.config import *
 
+# Builds the server configuration
+if os.getenv('IP'):
+  IP    = os.getenv('IP')
+else:
+  IP    = '0.0.0.0'
+
 # Run the web application.
 app.run(debug = config.sys.debug, host = config.sys.host, port = config.sys.port)
